c5b7f44308be180e069918f5d4ebc56251816a6a
SUENAGA Hiroki <hiroki.suenaga@gmail.com>
2013-08-28 20:05:49 +0900

    Add OpenBPF

---
+++ OpenBPF/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  OpenBPF
+//
+//  Created by SUENAGA Hiroki on 2013/08/28.
+//  Copyright (c) 2013年 SUENAGA Hiroki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
